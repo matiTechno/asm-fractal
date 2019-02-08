988bd84e4e4ba93e166c42fc5bbad7cc5565c1d3
matiTechno <mateusz.macieja8@gmail.com>
2019-02-08 08:57:08 +0100

    creating ppm in asm and rendering plain color - but some bug is introduced and colors are not correct

--- render.asm
+++ render.asm
@@ -1,102 +1,63 @@
-%define WIDTH 640
-%define HEIGHT 480
-
 section .bss
-    buffer: resb 3 * WIDTH * HEIGHT ; 3 bytes per pixel
+
+buffer resb 640 * 480 * 3
+fd resd 1
+
+section .data
+
+filename db "render.ppm", 0
+tmp_header db "P6 640 480 255 "
+tmp_header_size dw 15
+
+global _start
 
 section .text
-    global _start
-
-
-
 _start:
 
-    mov r10, 0              ; current idx
-    mov r11, WIDTH * HEIGHT ; number of pixels
+    ; todo:
+    ; width and height as the arguments
+    ; allocate buffer on a heap
+    
+    mov rax, buffer
 
-compute:
-    cmp r10, r11
-    jne write_to_file
+render:
+    mov byte [rax], 255
+    mov byte [rax + 1], 0
+    mov byte [rax + 2], 255
 
-    ; write color to buffer at current index
-    ; should I add or subtract from buffer?
-    mov rax, r10
-    mul 3
-    mov r12, buffer
-    add r12, rax
-
-    ; r12 contains fist component of a pixel at current index
-    mov [r12]    , 125
-    mov [r12 + 1], 125
-    mov [r12 + 2], 50
-
-    inc r10
-    jmp compute
-
-
-
-
-
-
-write_to_file:
+    add rax, 3
+    cmp rax, buffer + 640 * 480 * 3
+    jne render
 
     ; open file
-    ; write header
-
-    mov r10, 0              ; current idx
-    mov r11, WIDTH * HEIGHT ; number of pixels
-
-write_pixel:
-    cmp r10, r11
-    jne end_file
-
-
-    ; write pixel to file
-    ; temp: we will write to stdout
-    mov rax, r10
-    mul 3
-    mov r13, buffer
-    add r13, rax
-
-        ; red
-        mov r12, 0
-        mov rax, [r13]
-        jmp int_to_str ; be careful with this - we don't want to overflow the stack
-        jmp print
-
-    inc r10
-    jmp write_pixel
-
-end_file:
-    ; close file
-    jmp exit
-    
-
-
-
-
-exit:
-    mov rax, 60 ; exit system call
-    mov rdi, 0  ; return 0
+    mov rax, 2
+    mov rdi, filename
+    mov rsi, 1101o ; truncate, create, write only
+    mov rdx, 644o  ; mode (permissions)
     syscall
 
+    mov [fd], eax ; save the file descriptor
 
+    ; write to file - header
+    mov rax, 1
+    mov edi, dword [fd]
+    mov rsi, tmp_header
+    mov edx, [tmp_header_size]
+    syscall
 
-; set rax to a target value
-; r12 must be set to 0; returns the size of a created string
-; rsp will point at a beginning of a string
+    ; write to file - buffer
+    mov rax, 1
+    mov edi, dword [fd]
+    mov rsi, buffer
+    mov rdx, 640 * 480 * 3
+    syscall
 
-int_to_str:
-    mov rdx, 0
-    mov rbx, 10
-    div rbx  ; this will divide rax by rbx and put the reminder in rdx and whole part
-             ;    in rax
-
-    add rdx, 48 ; convert to ascii code
-    push rdx
-    inc r12     ; r12 - size of a created string
-    cmp rax, 0
-    jne int_to_str
-
-
-print:
+    ; close file
+    mov rax, 3
+    mov edi, dword [fd]
+    syscall
+    
+    ; exit
+    mov rax, 60
+    mov rdi, 0
+    syscall
